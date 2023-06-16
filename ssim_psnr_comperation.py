4b4b0e3d988fae226cb9dc1fbc47c327b4b53b6f
Enes Güler <kriptoenes@gmail.com>
2023-06-16 17:32:58 +0300

    ssim psnr comparetion updated

--- ssim_psnr_comperation.py
+++ ssim_psnr_comperation.py
@@ -3,6 +3,7 @@ from skimage import io, metrics
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from image_similarity_measures.evaluate import evaluation
+# !python3 -m pip install image-similarity-measures pyfftw
 
 # Create empty lists to store the PSNR and SSIM values for each model
 psnr_model1_list = []
@@ -12,30 +13,44 @@ ssim_model1_list = []
 ssim_model2_list = []
 ssim_model3_list = []
 
-def evaluate(gt_path, model1_path, model2_path, model3_path):
+# Define the paths to the ground truth and model image folders
+num_of_img_per_scene = 200
+scene_names = "army_base", "bazaar-indoor", "bazaar-outdoor", "classroom", "forest_road", "kitchen", "ski_slope", "sky", "soccer_field", "wave"
+for scene in scene_names:
+
+    gt_path = f'/content/drive/MyDrive/ColorizationGAN/TEST/final_outputs/{scene}'
+    model1_path = f'/content/drive/MyDrive/ColorizationGAN/TEST/final_outputs/{scene}'
+    model2_path = f'/content/drive/MyDrive/ColorizationGAN/TEST/final_outputs/{scene}_out'
+    model3_path = f'/content/drive/MyDrive/ColorizationGAN/TEST/final_outputs/{scene}_out'
+
     # Loop through each image in the folder
-    for filename in tqdm(os.listdir(model2_path)):
-        if "eccv16" in model2_path:
+    for filename in tqdm(os.listdir(model2_path)[:num_of_img_per_scene]):
+        if "ipynb" in filename:
           continue
+        # if "eccv16" in model2_path:
+        #   continue
         num = filename.split('_')[0]
 
         # Load the ground truth image
         gt_img = io.imread(os.path.join(gt_path, f"{num}_gt.jpg"))
 
+        eccv_postname = "_gt_eccv16.png"
+        siggraph_postname = "_gt_siggraph17.png"
+
         # Load the three model images
         model1_img = io.imread(os.path.join(model1_path, f"{num}_generated.jpg"))
-        model2_img = io.imread(os.path.join(model2_path, f"{num}_gt_eccv16.png"))
-        model3_img = io.imread(os.path.join(model3_path, f"{num}_gt_siggraph17.png"))
+        model2_img = io.imread(os.path.join(model2_path, f"{num}{eccv_postname}"))
+        model3_img = io.imread(os.path.join(model3_path, f"{num}{siggraph_postname}"))
 
         # Compute PSNR for each model image
         psnr_model1 = evaluation(org_img_path=os.path.join(gt_path, f"{num}_gt.jpg"), pred_img_path= os.path.join(model1_path, f"{num}_generated.jpg"), metrics=["psnr"])["psnr"]
-        psnr_model2 = evaluation(org_img_path=os.path.join(gt_path, f"{num}_gt.jpg"), pred_img_path= os.path.join(model2_path, f"{num}_gt_eccv16.png"), metrics=["psnr"])["psnr"]
-        psnr_model3 = evaluation(org_img_path=os.path.join(gt_path, f"{num}_gt.jpg"), pred_img_path= os.path.join(model3_path, f"{num}_gt_siggraph17.png"), metrics=["psnr"])["psnr"]
+        psnr_model2 = evaluation(org_img_path=os.path.join(gt_path, f"{num}_gt.jpg"), pred_img_path= os.path.join(model2_path, f"{num}{eccv_postname}"), metrics=["psnr"])["psnr"]
+        psnr_model3 = evaluation(org_img_path=os.path.join(gt_path, f"{num}_gt.jpg"), pred_img_path= os.path.join(model3_path, f"{num}{siggraph_postname}"), metrics=["psnr"])["psnr"]
 
         # Compute SSIM for each model image
         ssim_model1 = evaluation(org_img_path=os.path.join(gt_path, f"{num}_gt.jpg"), pred_img_path= os.path.join(model1_path, f"{num}_generated.jpg"), metrics=["ssim"])["ssim"]
-        ssim_model2 = evaluation(org_img_path=os.path.join(gt_path, f"{num}_gt.jpg"), pred_img_path= os.path.join(model2_path, f"{num}_gt_eccv16.png"), metrics=["ssim"])["ssim"]
-        ssim_model3 = evaluation(org_img_path=os.path.join(gt_path, f"{num}_gt.jpg"), pred_img_path= os.path.join(model3_path, f"{num}_gt_siggraph17.png"), metrics=["ssim"])["ssim"]
+        ssim_model2 = evaluation(org_img_path=os.path.join(gt_path, f"{num}_gt.jpg"), pred_img_path= os.path.join(model2_path, f"{num}{eccv_postname}"), metrics=["ssim"])["ssim"]
+        ssim_model3 = evaluation(org_img_path=os.path.join(gt_path, f"{num}_gt.jpg"), pred_img_path= os.path.join(model3_path, f"{num}{siggraph_postname}"), metrics=["ssim"])["ssim"]
 
         # Append the PSNR and SSIM values to the lists
         psnr_model1_list.append(psnr_model1)
@@ -47,17 +62,8 @@ def evaluate(gt_path, model1_path, model2_path, model3_path):
 
 
 
-scenes = ["forest_road", "army_base", "sky", "wave"]
-for scene in scenes:
-    # Define the paths to the ground truth and model image folders
-    gt_path = f'/content/drive/MyDrive/ColorizationGAN/TEST/vize_{scene}'
-    model1_path = f'/content/drive/MyDrive/ColorizationGAN/TEST/vize_{scene}'
-    model2_path = f'/content/drive/MyDrive/ColorizationGAN/TEST/vize_{scene}_out'
-    model3_path = f'/content/drive/MyDrive/ColorizationGAN/TEST/vize_{scene}_out'
-    evaluate(gt_path, model1_path, model2_path, model3_path)
-
 # Create a plot for PSNR
-plt.figure().set_figwidth(30)
+plt.figure().set_figwidth(20)
 plt.plot(psnr_model1_list, label='Our Model')
 plt.plot(psnr_model2_list, label='ECCV16')
 plt.plot(psnr_model3_list, label='SIGGRAPH17')
@@ -66,7 +72,7 @@ plt.legend()
 plt.show()
 
 # Create a plot for SSIM
-plt.figure().set_figwidth(30)
+plt.figure().set_figwidth(20)
 plt.plot(ssim_model1_list, label='Our Model')
 plt.plot(ssim_model2_list, label='ECCV16')
 plt.plot(ssim_model3_list, label='SIGGRAPH17')
